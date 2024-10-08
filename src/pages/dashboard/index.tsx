30b2abb9ce962a9a58cd0bfbe3a2dbd34ce4793e
JeuVictor <jeu.victor14@gmail.com>
2024-10-08 20:55:05 -0300

    redirecionando para tela principal usuarios deslogados

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,12 @@
 import Head from 'next/head';
 import styles from './styles.module.css';
+import { GetServerSideProps } from 'next';
+import {getSession} from 'next-auth/react';
 
 export default function Dashboard(){
+
+
+
     return(
         
         <div className={styles.container}>
@@ -15,4 +20,22 @@ export default function Dashboard(){
 
     )
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const session = await getSession({req});
+    
+    if(!session?.user){
+        return {
+                redirect:{
+                    destination: '/',
+                    permanent: false,
+            },
+        };
+    }
+
+
+    return{
+        props: {},
+    };
+};
 
