d17e26ae5f979628b06ac18a89c06e1ab6646d39
JeuVictor <jeu.victor14@gmail.com>
2024-10-07 21:37:37 -0300

    Criando funcionalidade ao login google

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,12 @@
+import {useSession, signIn, signOut } from 'next-auth/react';
 import styles from "./styles.module.css";
 import Link from "next/link";
+import React from 'react';
 
 export function Header(){
+
+    const {data: session, status} = useSession();
+
     return(
         <header className={styles.header}>
             <section className={styles.content}>
@@ -9,9 +14,23 @@ export function Header(){
                     <Link href="/">
                         <h1 className={styles.logo}>Tarefas <span>+</span></h1>
                     </Link>
-                    <Link href="/dashboard" className={styles.link} >Meu painel</Link>
+                    {
+                        session?.user && (
+                            <Link href="/dashboard" className={styles.link} >Meu painel</Link>)
+                    }
                 </nav>
-                <button className={styles.loginButton}>Acessar</button>
+                
+                {
+                    status === "loading" ? (
+                    <></>
+                ) : session ?(
+                    <button className={styles.loginButton} onClick={()=> signOut()} >
+                        Olá, {session?.user?.name}.
+                    </button>):(
+                    <button className={styles.loginButton} onClick={()=> signIn("google")} >
+                        Acessar
+                    </button>
+                    )}
             </section>
         </header>
     )
