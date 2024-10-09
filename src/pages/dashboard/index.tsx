88659c868ef91ffaf24bc75e8011acfdf981b168
JeuVictor <jeu.victor14@gmail.com>
2024-10-09 14:37:32 -0300

    adicionando campos que irão  receber do BD

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,6 +3,8 @@ import styles from './styles.module.css';
 import { GetServerSideProps } from 'next';
 import {getSession} from 'next-auth/react';
 import { Textarea } from '../../components/textarea';
+import { FiShare2, FiShare, FiSend } from 'react-icons/fi';
+import { FaTrash } from 'react-icons/fa';
 
 export default function Dashboard(){
 
@@ -29,10 +31,29 @@ export default function Dashboard(){
                             </div>
                             <button className={styles.button} type='submit'> Registrar </button>
                         </form>
-                
-
                     </div>
                 </section>
+
+                <section className={styles.taskContainer}>
+                    <h1>Minhas tarefas</h1>
+                    <article className={styles.task}>
+                        <div className={styles.tagContainer}>
+                            <label className={styles.tag}>PUBLICO</label>
+                            <button className={styles.shareButton}>
+                                <FiShare2 size={22} color='#3183ff'/>
+                            </button>
+                        </div>
+                        <div className={styles.taskContent}>
+                            <p>Minha primeira tarefa de exemplo.</p>
+                            <button className={styles.trash}>
+                                <FaTrash size={24} color='#ea3140'/>
+                            </button>
+                        </div>
+                    </article>
+
+                </section>
+
+                
             </main>        
         </div>
 
